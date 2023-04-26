9875578f1da268821e3bc3bbd218a257ff167de0
FurkannnAtes <aliriza20005@hotmail.com>
2023-04-26 19:51:00 +0300

    seo settings

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,11 +3,15 @@ import MenuGrills from "@/components/home/MenuGrills";
 import MenuHamburgers from "@/components/home/MenuHamburgers";
 import MenuSalads from "@/components/home/MenuSalads";
 import MenuSoups from "@/components/home/MenuSoups";
+import Head from "next/head";
 import React from "react";
 
 const index = () => {
   return (
     <div className="scroll-smooth">
+      <Head>
+        <title>Ofenos</title>
+      </Head>
       <Banner />
       <MenuHamburgers />
       <MenuSoups />
